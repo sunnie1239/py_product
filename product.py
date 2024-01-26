f49eb7fc31e1a620dfd555f3f9f7438317e31e2d
Sunnie Lin <sunnie1239@gmail.com>
2024-01-26 14:31:51 -0800

    read file

--- product.py
+++ product.py
@@ -1,11 +1,19 @@
 product = []
 
+# read file
+with open('product.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'Item 商品, Price 價格' in line:
+			continue # 執行下一次迴圈
+		[name, price] = line.strip().split(',')
+		product.append([name, price])
+
 # enter new product
 while True:
 	name = input('Please enter product name: ')
 	if name == 'q':
 		break
-	price = input('Please enter product price: ')	
+	price = input('Please enter product price: ')
 	product.append([name, price])	
 
 # print products on screen
@@ -16,4 +24,4 @@ for d in product:
 with open('product.csv', 'w', encoding='utf-8') as f:
 	f.write('Item 商品, Price 價格\n')
 	for d in product:
-		f.write(d[0] + ',' + d[1] + '\n')	
+		f.write(d[0] + ',' + d[1] + '\n')
