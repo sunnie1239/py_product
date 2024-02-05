8b4ddd1438c91184ad0ede3286097436397c669b
Sunnie Lin <sunnie1239@gmail.com>
2024-02-05 00:54:52 -0800

    refactor

--- product.py
+++ product.py
@@ -1,34 +1,48 @@
 import os
 
-product = []
-
-# check file exist or not
-if os.path.isfile('product.csv'):
-	print("There is product.csv file.")
-	# read file
-	with open('product.csv', 'r', encoding='utf-8') as f:
+# read file
+def read_file(filename):
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if 'Item 商品, Price 價格' in line:
 				continue # 執行下一次迴圈
 			[name, price] = line.strip().split(',')
 			product.append([name, price])
-else:
-	print("There's no product.csv file.")			
+	return product		
+						
 
 # enter new product
-while True:
-	name = input('Please enter product name: ')
-	if name == 'q':
-		break
-	price = input('Please enter product price: ')
-	product.append([name, price])	
+def user_input(product):
+	while True:
+		name = input('Please enter product name: ')
+		if name == 'q':
+			break
+		price = input('Please enter product price: ')	
+		product.append([name, price])
+	return product	
 
-# print products on screen
-for d in product:
-	print('Name: ', d[0], 'Price: ', d[1])
+
+# print products
+def print_product(product):
+	for d in product:
+		print('Name: ', d[0], 'Price: ', d[1])
+
 
 # write file
-with open('product.csv', 'w', encoding='utf-8') as f:
-	f.write('Item 商品, Price 價格\n')
-	for d in product:
-		f.write(d[0] + ',' + d[1] + '\n')
+def write_file(filename, product):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('Item 商品, Price 價格\n')
+		for d in product:
+			f.write(d[0] + ',' + d[1] + '\n')
+
+
+filename = 'product.csv'
+product = []
+# check file exist or not
+if os.path.isfile(filename):	
+	product = read_file(filename)
+else:
+	print('Cannot find the file!')	
+product = user_input(product)
+print_product(product)
+write_file(filename, product)	
